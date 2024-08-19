901eec36929b83461f8efd688e9e404ab01f1728
RavinduHarshana <ravinduharshana2001@gmail.com>
2024-08-19 08:40:08 +0530

    FInish Scroll Reveal

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const scrollRevealOption = {
 
 ScrollReveal().reveal(".hero-content", {
   ...scrollRevealOption,
+  delay: 500,
 });
 
 ScrollReveal().reveal(".cards", {
@@ -16,4 +17,47 @@ ScrollReveal().reveal(".cards", {
 ScrollReveal().reveal(".journybox", {
   ...scrollRevealOption,
   delay: 800,
+});
+
+ScrollReveal().reveal(".feuturedesc", {
+  ...scrollRevealOption,
+  delay: 800,
+});
+
+ScrollReveal().reveal(".c1", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 600,
+});
+ScrollReveal().reveal(".c2", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 700,
+});
+ScrollReveal().reveal(".c3", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 800,
+});
+ScrollReveal().reveal(".c4", {
+  ...scrollRevealOption,
+  delay: 900,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".cate", {
+  ...scrollRevealOption,
+  origin: "right",
+  delay: 800,
+});
+
+ScrollReveal().reveal(".reviewt  ", {
+  ...scrollRevealOption,
+
+  delay: 900,
+});
+
+ScrollReveal().reveal(".review  ", {
+  ...scrollRevealOption,
+  delay: 1100,
 });
